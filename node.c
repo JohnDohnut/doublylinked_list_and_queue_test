89368494519042518f37cde5da812ecf58e579a0
DeadPulse <lgj1089@gmail.com>
2020-08-16 20:06:41 +0900

    modified:   node.c

--- node.c
+++ node.c
@@ -6,20 +6,20 @@ node_t* node_init(){
 	if(node == NULL)
 		return NULL;
 	memset(node,0,sizeof(node_t));	
-	return node
+	return node;
 }
 
 int node_destroy(node_t* node){
 	if(node ==NULL)
-		return -1;
+		return PARAM_NULL;
 	free(node);
-	return 0;
+	return NO_ERR;
 }
 
 int node_get_data(node_t* node){
 	if(node == NULL)
-		return -1;
-	return node->data
+		return PARAM_NULL;
+	return node->data;
 }
 
 node_t* node_get_next(node_t* node){
@@ -36,23 +36,41 @@ node_t* node_get_prev(node_t* node){
 
 int node_set_data(node_t* node, int data){
 	if(node == NULL)
-		return -1;
+		return PARAM_NULL;
 	node->data = data;
-	return 0;
+	return NO_ERR;
 }
 
 int node_set_prev(node_t* node, node_t* prev){
 	if(node == NULL)
-		return -1;
+		return PARAM_NULL;
 	node->prev = prev;
-	return 0;
+	return NO_ERR;
 }
 
 int node_set_next(node_t* node, node_t* next){
 	if(node == NULL)
-		return -1;
+		return PARAM_NULL;
 	node->next = next;
-	return 0;
+	return NO_ERR;
 }
+
+int node_clear_node(node_t* node){
+	if(node == NULL)
+		return PARAM_NULL;
+	memset(node,0,sizeof(node_t));
+	return NO_ERR;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
